71aa6fab6f1b17f241a144055b580ed95c977ab3
PoKang Chen <pkang@changanus.com>
2020-07-10 14:42:41 -0400

    hi

---
+++ New_folder/build/CMakeFiles/try.exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/try.exe.dir/try.cpp.obj"
+  "libtry.exe.dll.a"
+  "try.exe.exe"
+  "try.exe.exe.manifest"
+  "try.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/try.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
